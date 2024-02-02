7c79aa2e97a3721558b1f10adbb65f57ab01c06f
SkylineUwU <112541432+SkylineUwU@users.noreply.github.com>
2024-02-03 00:09:58 +0300

    add tests

--- lab01_exp/testexp.cpp
+++ lab01_exp/testexp.cpp
@@ -4,7 +4,25 @@
 #include "exp.cpp"
 #include "consts.hpp"
 
+template<typename F>
+bool checker() {
+    F error;
+    F max_error = 0;
+    for (F x = -50; x <= 50; x++) {
+        if (x <= 0) {
+            error = std::abs(ADAAI::Exp(x) - std::exp(x));
+        } else {
+            error = std::abs(ADAAI::Exp(x) / std::exp(x) - 1.0);
+        }
+        if (error >= max_error) {
+            max_error = error;
+        }
+    }
+    return error <= ADAAI::c_Eps<F> * 10;
+}
 
 int main() {
-    return 0;
+    std::cout << checker<double>() << '\n';
+    std::cout << checker<float>() << '\n';
+    std::cout << checker<long double>() << '\n';
 }
