d5a03812cc0a0b8abbe03d667c5c107a63339d04
timurio111 <timur.saifutdinovv@gmail.com>
2024-02-02 18:46:19 +0300

    minor fixes

--- lab01_exp/exp.cpp
+++ lab01_exp/exp.cpp
@@ -30,50 +30,57 @@ template<>
 constexpr inline float sqrt2<long double> = (long double) M_SQRT2;
 
 template<typename F>
-constexpr F Exp(F x) {
-    static_assert(std::is_floating_point_v<F>);
-    F y;
-    y = x / ln2<F>;
-    F y0;
-    F y1 = std::modf(y, &y0); //y1 - fractional part, y0 - integral part
-    if (y1 > 0.5) {
-        y0 += 1;
-        y1 = y1 - 1;
-    } else if (y1 < -0.5) {
-        y0 -= 1;
-        y1 = y1 + 1;
+constexpr F Exp(F x)
+{
+  static_assert(std::is_floating_point_v<F>);
+  F y = NAN;
+  y = x / ln2<F>;
+  F y0 = NAN;
+  F y1 = std::modf(y, &y0); //y1 - fractional part, y0 - integral part
+  if (y1 > 0.5)
+  {
+    y0++;
+    y1--;
+  } 
+  else if (y1 < -0.5) 
+  {
+    y0--;
+    y1++;
+  }
+  int n = (int) y0;
+  if (n < INT_MIN) 
+    return 0.0;
+  else if (n > INT_MAX) 
+    return std::numeric_limits<F>::infinity();
+  else 
+  {
+    F delta = 10.0 * eps<F>;
+    F x1 = y1 * ln2<F>;
+    F e_x1 = NAN;
+    if (x1 > 0) 
+    {
+      for (int i = 0; i <= n; i++) 
+      {
+        e_x1 += pow(x1, i) / tgamma(i + 1); // tgamma(i+1) - to calculate i!
+        if (delta > e_x1 * sqrt2<F>)
+          break;
+      }
+      F e_x = NAN;
+      e_x = std::ldexp(e_x1, n); //e^x = e^x1 * 2^n
+      return e_x;
     }
-    int n = (int) y0;
-    if (n < INT_MIN) {
-        return 0.0;
-    } else if (n > INT_MAX) {
-        return std::numeric_limits<F>::infinity();
-    } else {
-        F delta = 10.0 * eps<F>;
-        F x1 = y1 * ln2<F>;
-        F e_x1;
-        if (x1 > 0) {
-            for (int i = 0; i <= n; i++) {
-                e_x1 += pow(x1, i) / tgamma(i + 1); // tgamma(i+1) - to calculate i!
-                if (delta > e_x1 * sqrt2<F>){
-                    break;
-                }
-            }
-            F e_x;
-            e_x = std::ldexp(e_x1, n); //e^x = e^x1 * 2^n
-            return e_x;
-        }
-        else if (x1 < 0){
-            for (int i = 0; i <= n; i++) {
-                e_x1 += pow(x1, i) / tgamma(i + 1); // tgamma(i+1) - to calculate i!
-                if (delta > e_x1){
-                    break;
-                }
-            }
-            F e_x;
-            e_x = std::ldexp(e_x1, n); //e^x = e^x1 * 2^n
-            return e_x;
-        }
+    else if (x1 < 0)
+    {
+      for (int i = 0; i <= n; i++) 
+      {
+        e_x1 += pow(x1, i) / tgamma(i + 1); // tgamma(i+1) - to calculate i!
+        if (delta > e_x1)
+          break;
+      }
+      F e_x = NAN;
+      e_x = std::ldexp(e_x1, n); //e^x = e^x1 * 2^n
+      return e_x;
     }
+  }
 }
 
